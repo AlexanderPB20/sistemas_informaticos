8b2e4263621535f396fabc02a74206162ad98432
jorgeCaller <jorge.caller@estudiante.uam.es>
2026-09-18 19:16:58 +0200

    Create user funcionando. Los usuarios se guardan en un diccionario por UUID como clave

--- user.py
+++ user.py
@@ -1,27 +1,41 @@
-from quart import Quart, jsonify
+from quart import Quart, jsonify, request
+import uuid
+import hashlib
 app = Quart(__name__)
 
+users = {}
+
+
 @app.put('/user')
-async def create_user(name: str,password: str):
+async def create_user():
+    #Tomado de: https://mojoauth.com/parse-and-generate-formats/parse-and-generate-json-with-quart#handling-incoming-json-requests
+    datos = await request.get_json()
+    if datos:
+        #Generacion del uuid
+        secret_uuid = uuid.uuid4()
+        stringified_uuid = str(secret_uuid)
+        
+        #Password hasheada
+        password = datos.get('password')
+        password256 = hashlib.sha256(password.encode()).hexdigest()
     
-    #Generacion del uuid
-    secret_uuid = uuid.uuid4()
-    stringified_uuid = str(secret_uuid)
-    
-    #Password hasheada
-    password256 = hashlib.sha256(password).hexdigest()
-    
-    entry = {"uuid": secret_uuid, "username" = name, "password"=password256}
-    return jsonify(entry)
+        users[stringified_uuid] = {"user":datos.get('name'),
+                        "password":password256}
+        # curl -X PUT "http://127.0.0.1:5050/user" -H "Content-Type: application/json" -d '{"name":"felipe","password":"mypassword"}'
+        
+        return users, 201
+    #Si no llega un json en la request
+    return {'error':'No se ha recibido JSON'}, 400
 
 @app.post('/user')
-async def login(name: str, password: str):
+async def login(name: str):
     #Login code goes here
+    return jsonify.loads(name)
 
 @app.patch('/user')
 async def modify(password: str):
     #Mofify password code goes here
-
+    return "Hello world"
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5050)
