deb53b1858562ccc4f950e9ea02d14a4ebdfcd13
jorgeCaller <jorge.caller@estudiante.uam.es>
2026-09-23 20:29:25 +0200

    Commit para guardar progreso. Estructura de file Básicamente he añadido el list_documents que ya lista todos los documentos del directorio y un intento de la peticion put

--- file.py
+++ file.py
@@ -6,20 +6,33 @@ from pathlib import Path
 app = Quart(__name__)
 
 
+def comprobar_uid(uid: str) -> bool:
+    path = Path('/file/',uid)
+    return path.exists()
+
+@app.get('/file/<uid>')
+async def list_documents(uid):
+    path = Path('./file/',uid)
+
+    if not comprobar_uid(uid):
+        return jsonify({'error':'El uid introducido no existe o no es un directorio'}),404
+    
+    file_list = [file.name for file in path.iterdir() if file.is_file()]
+    return jsonify({'files': file_list}), 200
+
+@app.put('/file/<uid>/<filename>')
+async def create_or_update(uid,filename):
+    #TODO contenido del fichero en el json??
+    #TODO Visibilidad. Separarlo por carpetas??
+    #TODO ^ revisar permisos del uid que accede? ^
+    path = Path('./file/',uid,"/",filename)
+    
+    if not comprobar_uid(uid)
+        return jsonify({'error':'El uid introducido no existe o no es un directorio'}),404
+
+    #Comprobar si ya existe el fichero, en tal caso actualizar
+
+
+
 if __name__ == '__main__':
     app.run(host='localhost', port=5050)
-
-
-@app.get('/user/<uid>')
-async def list_documents():
-    datos = await request.get_json()
-    if not datos:
-        #Si no llega un json en la request
-        return jsonify({'error':'No se ha recibido JSON'}), 400
-    if not datos.get('uid'):
-        #Si falta algún campo en el json
-        return jsonify({'error':'El JSON recibido debe contener un campo "uid".'}), 400
-    uid = datos.get('uid')
-    path = Path('./files/',uid)
-    if not path.exists():
-        return jsonify({'error':'El uid introducido no existe'}),404
